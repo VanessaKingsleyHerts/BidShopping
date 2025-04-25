ad5f194183f3ba8eb172ff8719f20676fb0303ce
VanessaKingsleyHerts <153731226+VanessaKingsleyHerts@users.noreply.github.com>
2025-04-25 22:20:18 +0200

    modify functional test for remote WebDriver

--- functional_tests/test_page.py
+++ functional_tests/test_page.py
@@ -2,16 +2,35 @@ from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 import time
+import os
 
 class TestHomePage(StaticLiveServerTestCase):
 
-    def setUp(self):
-        service = webdriver.ChromeService(executable_path='./functional_tests/chromedriver.exe')
-        self.browser = webdriver.Chrome(service=service)
-        
+    # def setUp(self):
+       # service = webdriver.ChromeService(executable_path='./functional_tests/chromedriver.exe')
+       # self.browser = webdriver.Chrome(service=service)
+    
+     def setUp(self):
+        # Read the remote URL from env (defaults to localhost if not set)
+        remote_url = os.environ.get('SELENIUM_REMOTE_URL', 'http://localhost:4444/wd/hub')
+
+        # Configure ChromeOptions for headless CI
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+
+        # Connect to the Selenium service instead of a local chromedriver.exe
+        self.browser = webdriver.Remote(
+            command_executor=remote_url,
+            options=options
+        )
+
+   # def tearDown(self):
+      #  self.browser.close()
 
     def tearDown(self):
-        self.browser.close()
+        self.browser.quit()
 
     def test_home(self):
         self.browser.get(self.live_server_url)
