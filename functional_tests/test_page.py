66553cde345d0fc326a48b1df8a6d5ddb2246fe9
VanessaKingsleyHerts <153731226+VanessaKingsleyHerts@users.noreply.github.com>
2025-04-26 14:09:08 +0200

    test

--- functional_tests/test_page.py
+++ functional_tests/test_page.py
@@ -5,6 +5,7 @@ from django.test import tag
 from selenium import webdriver
 from django.urls import reverse
 import time
+from urllib.parse import urlparse
 
 SCREENSHOT_DIR = os.path.join(os.getcwd(), 'functional_tests', 'screenshots')
 os.makedirs(SCREENSHOT_DIR, exist_ok=True)
@@ -23,6 +24,12 @@ class TestHomePage(StaticLiveServerTestCase):
             command_executor=os.environ['SELENIUM_REMOTE_URL'],
             options=options,
         )
+        # 🚀 Fix live server URL for Docker network!
+        if "CI" in os.environ:
+            parsed_url = urlparse(cls.live_server_url)
+            cls.remote_server_url = f"http://host.docker.internal:{parsed_url.port}"
+        else:
+            cls.remote_server_url = cls.live_server_url
 
     @classmethod
     def tearDownClass(cls):
@@ -30,31 +37,31 @@ class TestHomePage(StaticLiveServerTestCase):
         super().tearDownClass()
 
     def test_home(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.remote_server_url )
         time.sleep(1)
 
     def test_user_login(self):
-        url = self.live_server_url + reverse('login_user')
+        url = self.remote_server_url  + reverse('login_user')
         self.browser.get(url)
         time.sleep(1)
 
     def test_admin_login(self):
-        url = self.live_server_url + reverse('login_admin')
+        url = self.remote_server_url  + reverse('login_admin')
         self.browser.get(url)
         time.sleep(1)
 
     def test_about(self):
-        url = self.live_server_url + reverse('about')
+        url = self.remote_server_url  + reverse('about')
         self.browser.get(url)
         time.sleep(1)
 
     def test_contact(self):
-        url = self.live_server_url + reverse('contact')
+        url = self.remote_server_url  + reverse('contact')
         self.browser.get(url)
         time.sleep(1)
 
     def test_file_upload(self):
-        self.browser.get(self.live_server_url + reverse('upload_view'))
+        self.browser.get(self.remote_server_url  + reverse('upload_view'))
         upload_input = self.browser.find_element_by_name('file_field')
         # ensure you have a sample file in your repo
         upload_input.send_keys(os.path.join(os.getcwd(), 'functional_tests', 'fixtures', 'sample.pdf'))
@@ -64,7 +71,7 @@ class TestHomePage(StaticLiveServerTestCase):
         self.assertIn('uploaded', success.text.lower())
 
     def test_file_download_link(self):
-        self.browser.get(self.live_server_url + reverse('download_view'))
+        self.browser.get(self.remote_server_url  + reverse('download_view'))
         link = self.browser.find_element_by_tag_name('a')
         href = link.get_attribute('href')
         # you could issue a direct HTTP GET here to validate headers:
