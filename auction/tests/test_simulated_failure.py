755705fdfd66b9bcb395f720360dea2b0c605690
VanessaKingsleyHerts <153731226+VanessaKingsleyHerts@users.noreply.github.com>
2025-05-05 23:24:35 +0200

    create simulated failure

---
+++ auction/tests/test_simulated_failure.py
@@ -0,0 +1,6 @@
+from django.test import SimpleTestCase
+
+class SimulatedFailureTest(SimpleTestCase):
+    def test_forced_failure(self):
+        """Always fails – used to generate a failure log for our CI anomaly detector."""
+        self.fail("💥 Simulated CI failure for logging")
