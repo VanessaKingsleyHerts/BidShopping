a99856a076cf0f9665bfea7418b525b142c31890
VanessaKingsleyHerts <153731226+VanessaKingsleyHerts@users.noreply.github.com>
2025-08-01 10:47:20 +0200

    Update test_force_fail.py

--- auction/tests/test_force_fail.py
+++ auction/tests/test_force_fail.py
@@ -7,5 +7,5 @@ class SimulatedFailureTest(SimpleTestCase):
         # self.fail("💥 Simulated CI failure for logging")
 
     def test_failure_sim(self):
-    if os.environ.get("FAIL_NEXT") == "1":
+        if os.environ.get("FAIL_NEXT") == "1":
         self.assertEqual(1, 0)  # Force fail
